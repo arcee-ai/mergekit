5b2154c04b1f84fbc4e366146f9e6612b31dd44f
Charles Goddard <chargoddard@gmail.com>
2023-09-08 17:01:26 -0700

    Add type annotations + eps to slerp

--- merge_methods/slerp.py
+++ merge_methods/slerp.py
@@ -43,11 +43,19 @@ def slerp_merge_tensors(
     )
 
 
-def lerp(t, v0, v1):
+def lerp(
+    t: float, v0: Union[np.ndarray, torch.Tensor], v1: Union[np.ndarray, torch.Tensor]
+) -> Union[np.ndarray, torch.Tensor]:
     return (1 - t) * v0 + t * v1
 
 
-def slerp(t, v0, v1, DOT_THRESHOLD=0.9995):
+def slerp(
+    t: Union[float, np.ndarray],
+    v0: Union[np.ndarray, torch.Tensor],
+    v1: Union[np.ndarray, torch.Tensor],
+    DOT_THRESHOLD: float = 0.9995,
+    eps: float = 1e-8,
+):
     """
     Spherical linear interpolation
 
@@ -57,44 +65,58 @@ def slerp(t, v0, v1, DOT_THRESHOLD=0.9995):
         v0 (np.ndarray): Starting vector
         v1 (np.ndarray): Final vector
         DOT_THRESHOLD (float): Threshold for considering the two vectors as
-                               colineal. Not recommended to alter this.
+                               colinear. Not recommended to alter this.
     Returns:
         v2 (np.ndarray): Interpolation vector between v0 and v1
     """
-    c = False
+    is_torch = False
     if not isinstance(v0, np.ndarray):
-        c = True
+        is_torch = True
         v0 = v0.detach().cpu().float().numpy()
     if not isinstance(v1, np.ndarray):
-        c = True
+        is_torch = True
         v1 = v1.detach().cpu().float().numpy()
+
     # Copy the vectors to reuse them later
     v0_copy = np.copy(v0)
     v1_copy = np.copy(v1)
+
     # Normalize the vectors to get the directions and angles
-    v0 = v0 / np.linalg.norm(v0)
-    v1 = v1 / np.linalg.norm(v1)
+    v0 = normalize(v0, eps)
+    v1 = normalize(v1, eps)
+
     # Dot product with the normalized vectors (can't use np.dot in W)
     dot = np.sum(v0 * v1)
-    # If absolute value of dot product is almost 1, vectors are ~colineal, so use lerp
+
+    # If absolute value of dot product is almost 1, vectors are ~colinear, so use lerp
     if np.abs(dot) > DOT_THRESHOLD:
         res = lerp(t, v0_copy, v1_copy)
-        if c:
-            res = torch.from_numpy(res)
-        return res
+        return maybe_torch(res, is_torch)
 
     # Calculate initial angle between v0 and v1
     theta_0 = np.arccos(dot)
     sin_theta_0 = np.sin(theta_0)
+
     # Angle at timestep t
     theta_t = theta_0 * t
     sin_theta_t = np.sin(theta_t)
+
     # Finish the slerp algorithm
     s0 = np.sin(theta_0 - theta_t) / sin_theta_0
     s1 = sin_theta_t / sin_theta_0
-    v2 = s0 * v0_copy + s1 * v1_copy
-    if c:
-        res = torch.from_numpy(v2)
-    else:
-        res = v2
-    return res
+    res = s0 * v0_copy + s1 * v1_copy
+
+    return maybe_torch(res, is_torch)
+
+
+def maybe_torch(v: np.ndarray, is_torch: bool):
+    if is_torch:
+        return torch.from_numpy(v)
+    return v
+
+
+def normalize(v: np.ndarray, eps: float):
+    norm_v = np.linalg.norm(v)
+    if norm_v > eps:
+        v = v / norm_v
+    return v
