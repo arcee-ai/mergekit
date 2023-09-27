fa87a74410a7b44117ab02db8f5674065ec9bd87
Charles Goddard <chargoddard@gmail.com>
2023-09-26 20:55:32 -0700

    wip

--- bakllama.py
+++ bakllama.py
@@ -13,7 +13,7 @@ from pydantic import BaseModel
 from tqdm import tqdm
 
 from common import LLAMA_LAYER_MEMBERS, ModelReference
-from sharded_tensor_index import LazyTensorLoader
+from lazy_tensors import LazyTensorLoader, TensorWriter
 
 
 class LayerSlice(BaseModel):
@@ -27,84 +27,6 @@ class BakllamaConfig(BaseModel):
     layer_slices: List[LayerSlice]
     embedding_source: Optional[str] = None
     lm_head_source: Optional[str] = None
-
-
-class TensorWriter:
-    out_path: str
-    max_shard_size: int
-    shards_written: int
-    weight_map = Dict[str, str]
-    current_shard: Dict[str, torch.Tensor]
-    current_shard_size: int
-
-    def __init__(
-        self, out_path: str, max_shard_size: int = 1000 * 1000 * 1000 * 5
-    ) -> None:
-        os.makedirs(out_path, exist_ok=True)
-
-        self.out_path = out_path
-        self.max_shard_size = max_shard_size
-        self.shards_written = 0
-        self.weight_map = {}
-        self.current_shard = {}
-        self.current_shard_size = 0
-
-    def save_tensor(self, name: str, tensor: torch.Tensor, clone: bool = False):
-        tensor_size = tensor.view(-1).shape[0]
-        if (
-            self.current_shard
-            and self.current_shard_size + tensor_size > self.max_shard_size
-        ):
-            self.flush_current_shard()
-
-        if clone:
-            tensor = tensor.clone()
-
-        self.current_shard[name] = tensor
-        self.current_shard_size += tensor_size
-
-    def flush_current_shard(self):
-        if not self.current_shard:
-            return
-
-        shard_name = f"model-{self.shards_written+1}.safetensors"
-        for key in self.current_shard:
-            self.weight_map[key] = shard_name
-        safetensors.torch.save_file(
-            self.current_shard,
-            os.path.join(self.out_path, shard_name),
-            metadata={"format": "pt"},
-        )
-        self.current_shard = {}
-        self.current_shard_size = 0
-        self.shards_written = self.shards_written + 1
-
-    def finalize(self):
-        self.flush_current_shard()
-
-        # standardize shard names to hf format
-        total_shards = self.shards_written
-        name_remap = {}
-        for idx in range(total_shards):
-            name_remap[
-                f"model-{idx+1}.safetensors"
-            ] = f"model-{idx+1:05d}-of-{total_shards:05d}.safetensors"
-
-        for old_name, new_name in name_remap.items():
-            os.rename(
-                os.path.join(self.out_path, old_name),
-                os.path.join(self.out_path, new_name),
-            )
-
-        for key in self.weight_map:
-            self.weight_map[key] = name_remap[self.weight_map[key]]
-
-        with open(
-            os.path.join(self.out_path, "model.safetensors.index.json"),
-            "w",
-            encoding="utf-8",
-        ) as file:
-            json.dump({"metadata": {}, "weight_map": self.weight_map}, file)
 
 
 def process(
