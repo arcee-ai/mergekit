1b01f769aa8889ae166e726f3fa7ec199d8685c9
Charles Goddard <chargoddard@gmail.com>
2023-09-28 13:09:21 -0700

    Fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from typing_extensions import Annotated
 
 import common
 import merge_methods
-from config import MergeConfiguration
+from config import MergeConfiguration, OutputSliceDefinition
 from common import LLAMA_INFO, ModelReference
 from graph import Executor, RuleSet
 from plan import plan
@@ -30,40 +30,30 @@ def main(
         ),
     ] = False,
     copy_tokenizer: Annotated[
-        bool, typer.Option("Copy a tokenizer to the output")
+        bool, typer.Option(help="Copy a tokenizer to the output")
     ] = True,
 ):
     with open(config_file, "r", encoding="utf-8") as file:
         data = yaml.load(file, yaml.SafeLoader)
 
-    config = MergeConfiguration.parse_obj(data)
-    (targets, static_rules) = plan(config, LLAMA_INFO)
+    merge_config = MergeConfiguration.parse_obj(data)
+    (targets, static_rules) = plan(merge_config, LLAMA_INFO)
 
     dtype: Optional[torch.dtype] = {
         None: None,
         "float16": torch.float16,
         "bfloat16": torch.bfloat16,
         "float32": torch.float32,
-    }[config.dtype]
+    }[merge_config.dtype]
 
-    # if models to merge are specified instead of output slices, compute them
-    if config.models:
-        if config.slices:
-            raise RuntimeError("Must specify either models to merge or output slices")
-        
-        config.slices = []
-        for model in config.models:
-            model_cfg = ModelReference.parse(model.model)
-
-
-    if not config.slices:
+    if not merge_config.slices:
         raise RuntimeError("No output requested")
 
-    method = merge_methods.get(config.merge_method)
+    method = merge_methods.get(merge_config.merge_method)
 
     rules = RuleSet(static_rules)
     exec = Executor(
-        config.referenced_models(),
+        merge_config.referenced_models(),
         targets,
         rules,
         {"merge": method},
@@ -74,10 +64,10 @@ def main(
     )
     exec.run(out_path)
 
-    method.model_out_config(config).save_pretrained(out_path)
+    method.model_out_config(merge_config).save_pretrained(out_path)
     if copy_tokenizer:
         try:
-            method.model_tokenizer(config).save_pretrained(
+            method.model_tokenizer(merge_config).save_pretrained(
                 out_path, safe_serialization=True
             )
         except Exception as e:
