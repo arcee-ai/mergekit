8edd67d416e4b3c55f05124c719a60d16b51ee20
Charles Goddard <chargoddard@gmail.com>
2024-01-02 12:01:24 -0800

    Bump copyright date to 2024

--- mergekit/io/lazy_unpickle.py
+++ mergekit/io/lazy_unpickle.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Charles O. Goddard
+# Copyright (C) 2024 Charles O. Goddard
 #
 # This software is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License as
