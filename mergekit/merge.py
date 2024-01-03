fb932d89f57760801dec86441adfd1cbd29b09e3
Charles O. Goddard <chargoddard@gmail.com>
2024-01-03 13:33:22 -0800

    Generate Hugging Face model card (#58)
    Output a README.md containing basic information about the merge.

--- mergekit/merge.py
+++ mergekit/merge.py
@@ -14,6 +14,7 @@
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
 import logging
+import os
 from typing import Optional
 
 import torch
@@ -22,6 +23,7 @@ from pydantic import BaseModel
 
 from mergekit import merge_methods
 from mergekit.architecture import get_architecture_info
+from mergekit.card import generate_card
 from mergekit.common import ModelReference, parse_kmb
 from mergekit.config import MergeConfiguration
 from mergekit.graph import Executor, RuleSet
@@ -41,9 +43,15 @@ class MergeOptions(BaseModel):
     trust_remote_code: bool = False
     random_seed: Optional[int] = None
     lazy_unpickle: bool = False
+    write_model_card: bool = True
 
 
-def run_merge(merge_config: MergeConfiguration, out_path: str, options: MergeOptions):
+def run_merge(
+    merge_config: MergeConfiguration,
+    out_path: str,
+    options: MergeOptions,
+    config_source: Optional[str] = None,
+):
     dtype: Optional[torch.dtype] = {
         None: None,
         "float16": torch.float16,
@@ -129,6 +137,23 @@ def run_merge(merge_config: MergeConfiguration, out_path: str, options: MergeOpt
             exc_info=e,
         )
     cfg_out.save_pretrained(out_path)
+
+    if options.write_model_card:
+        if not config_source:
+            config_source = merge_config.to_yaml()
+
+        card_md = generate_card(
+            config=merge_config,
+            config_yaml=config_source,
+            name=os.path.basename(out_path),
+        )
+        with open(os.path.join(out_path, "README.md"), "w", encoding="utf-8") as fp:
+            fp.write(card_md)
+
+        with open(
+            os.path.join(out_path, "mergekit_config.yml"), "w", encoding="utf-8"
+        ) as fp:
+            fp.write(config_source)
 
     if options.copy_tokenizer and tokenizer is None:
         try:
