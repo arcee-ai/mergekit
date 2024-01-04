134af396fd0534af04725270e80001efa2644a9a
Carsten <carsten@kragelund.me>
2024-01-04 20:21:26 +0000

    feat(mega): use name in yaml and out_path cli arg instead of out_path in yaml

--- mergekit/scripts/megamerge.py
+++ mergekit/scripts/megamerge.py
@@ -6,13 +6,12 @@ import re
 import click
 import logging
 import os
+from pathlib import Path
 
 from mergekit.merge import MergeOptions, run_merge
 from mergekit.config import MergeConfiguration
 from mergekit.options import add_merge_options
 
-# Regex that matches huggingface path
-hf_path = r"^[a-zA-Z0-9\-]+/[a-zA-Z0-9\-\._]+(?:\+.+)$"
 merges = {}
 
 def has_circular_dependency(nodes):
@@ -40,31 +39,33 @@ def has_circular_dependency(nodes):
 
     return None 
 
-def merge(m, options, force):
-    # check if out_path exists
-    if not force and os.path.exists(m):
-        logging.info(f"Skipping {m} as it already exists")
-        del merges[m]
-        return
-    elif force and os.path.exists(m):
-        logging.info(f"Overwriting {m} as --force was specified")
+def merge(m, merge_options, force, out_path):
+    # check if output_path exists
+    if os.path.exists(out_path / m):
+        if not force:
+            logging.info(f"Skipping {m} as it already exists")
+            del merges[m]
+            return
+        else:
+            logging.info(f"Overwriting {m} as --force was specified")
 
     if len(merges[m]["deps"]) != 0:
         for dep in merges[m]["deps"]:
             if dep in merges:
-                merge(dep, options, force)
+                merge(dep, merge_options, force, out_path)
 
     logging.info(f"Merging model {m}")
     merge_config: MergeConfiguration = MergeConfiguration.model_validate(merges[m])
     run_merge(
         merge_config,
-        merges[m]["out_path"],
-        options=options,
+        str(out_path / merges[m]["name"]),
+        options=merge_options,
     )
     del merges[m]
 
 @click.command("mergekit-mega")
 @click.argument("config_file")
+@click.argument("out_path")
 @click.option(
     "--verbose", "-v", type=bool, default=False, is_flag=True, help="Verbose logging"
 )
@@ -75,30 +76,46 @@ def merge(m, options, force):
 def main(
     merge_options: MergeOptions,
     config_file: str,
+    out_path: str,
     force: bool,
     verbose: bool,
 ):
     logging.basicConfig(level=logging.INFO if verbose else logging.WARNING)
 
+    out_path = Path(out_path)
     with open(config_file, "r") as f:
         data = yaml.load_all(f, Loader=yaml.FullLoader)
 
         for d in data:
-            merges[d["out_path"]] = d
-            merges[d["out_path"]]["deps"] = []
+            merges[d["name"]] = d
+            merges[d["name"]]["deps"] = []
             if "slices" in d:
                 for slc in d["slices"]:
                     for src in slc["sources"]:
                         if "model" in src and src["model"] is not None:
-                            # if the model is a huggingface model, skip it
-                            if not re.match(hf_path, src["model"]):
-                                merges[d["out_path"]]["deps"].append(src["model"])
+                            model_lora = src["model"].split("+")
+                            # name must not have a slash to avoid path traversal
+                            # therefore, we can use it to check if its a merge from the config
+                            if "/" not in model_lora[0]:
+                                # avoid duplicate deps
+                                if model_lora[0] not in merges[d["name"]]["deps"]:
+                                    merges[d["name"]]["deps"].append(model_lora[0])
+                                src["model"] = str(out_path / model_lora[0])
+                                if len(model_lora) == 2:
+                                    src["model"] += "+" + model_lora[1]
             if "models" in d:
                 for mdl in d["models"]:
                     if "model" in mdl and mdl["model"] is not None:
-                        # if the model is a huggingface model, skip it
-                        if not re.match(hf_path, mdl["model"]):
-                            merges[d["out_path"]]["deps"].append(mdl["model"])
+                        model_lora = mdl["model"].split("+")
+                        # name must not have a slash to avoid path traversal
+                        # therefore, we can use it to check if its a merge from the config
+                        if "/" not in model_lora[0]:
+                            # avoid duplicate deps
+                            if model_lora[0] not in merges[d["name"]]["deps"]:
+                                merges[d["name"]]["deps"].append(model_lora[0])
+                            mdl["model"] = str(out_path / model_lora[0])
+                            if len(model_lora) == 2:
+                                mdl["model"] += "+" + model_lora[1]
 
     logging.info("Merging: " + ', '.join(merges))
 
