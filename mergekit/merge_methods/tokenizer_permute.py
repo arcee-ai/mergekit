6c7dca15f82fafdee00b141edca21769e60feac6
Charles O. Goddard <chargoddard@gmail.com>
2024-01-04 16:08:59 -0800

    Tokenizer merge fix (#73)
    Addresses some situations where merging models with unused tokens can
    produce NaN entries in lm_head/embed_tokens.
    
    Also greatly reduces memory usage of tokenizer merging. Materializing
    the full permutation matrix for a Yi model with 64K tokens was costing
    ~16gb per model. Just storing an index remapping dictionary reduces this
    to ~512kb.

--- mergekit/merge_methods/tokenizer_permute.py
+++ mergekit/merge_methods/tokenizer_permute.py
@@ -28,7 +28,7 @@ class TokenizerPermutationMerge(MergeMethod):
     def __call__(
         self,
         input_tensors: Dict[TensorReference, torch.Tensor],
-        embed_permutations: Dict[ModelReference, torch.IntTensor],
+        embed_permutations: Dict[ModelReference, Dict[int, int]],
         config: ConfigReader,
         **_kwargs,
     ) -> torch.Tensor:
@@ -47,15 +47,20 @@ class TokenizerPermutationMerge(MergeMethod):
             models.append(tr.model)
 
             x = input_tensors[tr]
-            p = embed_permutations[tr.model].to(dtype=x.dtype, device=x.device)
-            temp_dtype = torch.float32 if x.device.type == "cpu" else x.dtype
-            if p.shape[1] == x.shape[0]:
-                xp = (p.to(dtype=temp_dtype) @ x.to(dtype=temp_dtype)).to(x.dtype)
-            else:
-                raise RuntimeError("Shape mismatch")
+            p = embed_permutations[tr.model]
+
+            xp = torch.zeros((len(p), x.shape[-1]), dtype=x.dtype, device=x.device)
+            mask = torch.zeros((len(p),), dtype=torch.bool, device=x.device)
+            for out_idx in p:
+                in_idx = p[out_idx]
+                if in_idx < 0:
+                    continue
+
+                xp[out_idx, :] = x[in_idx, :]
+                mask[out_idx] = 1
 
             expanded.append(xp)
-            masks.append(p.sum(dim=-1, keepdim=True) > 0)
+            masks.append(mask)
 
             is_base = tr.model == config.base_model
             if use_slerp:
@@ -63,11 +68,10 @@ class TokenizerPermutationMerge(MergeMethod):
                 weight = (1.0 - t) if is_base else t
             else:
                 weight = config.parameter("weight", model=tr.model, default=1.0)
-
             weights.append(weight)
 
         expanded = torch.stack(expanded, dim=0)
-        masks = torch.stack(masks, dim=0)
+        masks = torch.stack(masks, dim=0).unsqueeze(-1)
         weights = (
             torch.tensor(weights, dtype=expanded.dtype, device=expanded.device)
             .unsqueeze(-1)
