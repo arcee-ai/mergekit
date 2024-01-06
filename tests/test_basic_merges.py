190cf2f74e398ecc22b4b1e087f627b36a02baae
Charles O. Goddard <chargoddard@gmail.com>
2024-01-06 12:56:08 -0800

    Add tokenizer merging tests (#79)
    Also fixes a bug uncovered by said tests.

--- tests/test_basic_merges.py
+++ tests/test_basic_merges.py
@@ -1,9 +1,7 @@
-import os
-import tempfile
 from typing import Dict, Optional
 
 import pytest
-from transformers import LlamaConfig, LlamaForCausalLM
+from common import make_picollama, run_and_check_merge
 
 from mergekit.config import (
     InputModelDefinition,
@@ -12,21 +10,6 @@ from mergekit.config import (
     OutputSliceDefinition,
     ParameterSetting,
 )
-from mergekit.io.lazy_tensor_loader import LazyTensorLoader, ShardedTensorIndex
-from mergekit.merge import MergeOptions, run_merge
-
-
-def make_picollama(path: str):
-    cfg = LlamaConfig(
-        vocab_size=64,
-        hidden_size=32,
-        intermediate_size=48,
-        num_attention_heads=16,
-        num_hidden_layers=2,
-    )
-    model = LlamaForCausalLM(cfg)
-    model.save_pretrained(path, safe_serialization=True)
-    return str(path)
 
 
 @pytest.fixture(scope="session")
@@ -44,14 +27,14 @@ def model_c(tmp_path_factory):
     return make_picollama(tmp_path_factory.mktemp("model_c"))
 
 
-class TestMerges:
+class TestBasicMerges:
     def test_gpt2_copy(self):
         config = MergeConfiguration(
             merge_method="passthrough",
             models=[InputModelDefinition(model="gpt2")],
             dtype="bfloat16",
         )
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_gpt2_stack(self):
         config = MergeConfiguration(
@@ -64,24 +47,24 @@ class TestMerges:
             ],
             dtype="bfloat16",
         )
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_linear_merge(self, model_a, model_b):
         config = self.two_model_config(model_a, model_b, merge_method="linear")
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_slerp_merge(self, model_a, model_b):
         config = self.two_model_config(
             model_a, model_b, merge_method="slerp", base_model=model_a
         )
         config.parameters = {"t": 0.35}
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_task_arithmetic_merge(self, model_a, model_b, model_c):
         config = self.two_model_config(
             model_a, model_b, merge_method="task_arithmetic", base_model=model_c
         )
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_ties_merge(self, model_a, model_b, model_c):
         config = self.two_model_config(
@@ -91,7 +74,7 @@ class TestMerges:
             base_model=model_c,
             params={"density": 0.3},
         )
-        self.run_and_check_merge(config)
+        run_and_check_merge(config)
 
     def test_dare_ties_merge(self, model_a, model_b, model_c):
         config = self.two_model_config(
@@ -101,28 +84,7 @@ class TestMerges:
             base_model=model_c,
             params={"density": 0.66},
         )
-        self.run_and_check_merge(config)
-
-    def run_and_check_merge(self, config: MergeConfiguration):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            run_merge(config, out_path=tmpdir, options=MergeOptions())
-            assert os.path.exists(
-                os.path.join(tmpdir, "model.safetensors.index.json")
-            ), "No index file for merge"
-            assert os.path.exists(
-                os.path.join(tmpdir, "config.json")
-            ), "No config json produced by merge"
-
-            # check for NaN in output
-            loader = LazyTensorLoader(
-                ShardedTensorIndex.from_disk(tmpdir), lazy_unpickle=False
-            )
-            tp = loader.index.tensor_paths
-            sorted_tensors = sorted(tp.keys(), key=lambda k: tp[k])
-            for tensor_name in sorted_tensors:
-                tensor = loader.get_tensor(tensor_name)
-                has_nan = tensor.view(-1).isnan().any()
-                assert not has_nan, "Output contains NaN"
+        run_and_check_merge(config)
 
     def two_model_config(
         self,
