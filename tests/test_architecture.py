40725e53cbc80db4f56dc6b0a2c47f32959df7ee
Luke Meyers <functor.soup@gmail.com>
2024-02-21 18:24:44 -0500

    Correct mistake

--- tests/test_architecture.py
+++ tests/test_architecture.py
@@ -8,7 +8,7 @@ class TestArchitecture:
     def test_set_overrides(self):
         cfg = AutoConfig.from_pretrained("gpt2")
         arch_info = get_architecture_info(cfg)
-        configured_arch_info = ConfiguredArchitectureInfo(arch_info, cfg)
+        configured_arch_info = ConfiguredArchitectureInfo(info=arch_info, config=cfg)
 
         overrides = {"a_${layer_index}": "b_${layer_index}"}
         new_config = configured_arch_info.set_overrides(overrides)
