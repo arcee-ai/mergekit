49dedfd5e11b19be9a8e36e9cde146e58f392e5b
Shamane Siri <gayal.sha92@gmail.com>
2024-04-08 21:58:04 +1200

    added exact ZipIt permute function

--- mergekit/scripts/dump_out_m_and_u.py
+++ mergekit/scripts/dump_out_m_and_u.py
@@ -8,8 +8,11 @@ import safetensors.torch
 import scipy
 import torch
 
-from mergekit.scripts.zipit_utils import CovarianceMetric, remove_pads
+from mergekit.scripts.zipit_utils import CovarianceMetric
 
+
+def remove_col(x, idx):
+    return torch.cat([x[:, :idx], x[:, idx+1:]], dim=-1)
 
 def match_tensors_permute(
     r=0.5,
@@ -71,6 +74,108 @@ def match_tensors_permute(
 
     return merge.T, unmerge, None, cost / merge.shape[0]
 
+def match_tensors_zipit(
+    metric, r=.5, a=0.3, b=.125, 
+    correlation_matrix=None,
+    **kwargs
+):
+    """
+    ZipIt! matching algorithm. Given metric dict, computes matching as defined in paper. 
+    Args:
+    - metric: dictionary containing metrics. This must contain either a covariance or cossim matrix, and 
+        must be [(num_models x model_feature_dim), (num_models x model_feature_dim)]. 
+    - r: Amount to reduce total input feature dimension - this is num_models x model_feature_dim. This function will
+        compute (un)merge matrix that goes from 
+        (num_models x model_feature_dim) -> (1-r)*(num_models x model_feature_dim) = merged_feature_dim.
+        E.g. if num_models=2, model_feature_dim=10 and r=.5, the matrix will map from 2x10=20 -> (1-.5)x2x10=10, or halve the 
+        collective feature space of the models.
+    - a: alpha hyperparameter as defined in Section 4.3 of our paper. 
+    - b: beta hyperparameter as defined in Section 4.3 of our paper.
+    - print_merges: whether to print computed (un)merge matrices.
+    - get_merge_value default False, returns the sum of correlations over all the merges which the algorithm made. 
+    - add_bias: whether to add a bias to the input. This should only be used if your module expects the input with bias offset.
+    returns:
+    - (un)merge matrices
+    """
+    if "covariance" in metric:
+        sims = correlation_matrix
+    elif "cossim" in metric:
+        sims = metric["cossim"]
+    O = sims.shape[0]
+    remainder = int(O * (1-r) + 1e-4)
+    permutation_matrix = torch.eye(O, O)#, device=sims.device)
+
+    torch.diagonal(sims)[:] = -torch.inf
+
+    num_models = int(1/(1 - r) + 0.5)
+    Om = O // num_models
+
+    original_model = torch.zeros(O, device=sims.device).long()
+    for i in range(num_models):
+        original_model[i*Om:(i+1)*Om] = i
+
+    to_remove = permutation_matrix.shape[1] - remainder
+    budget = torch.zeros(num_models, device=sims.device).long() + int((to_remove // num_models) * b + 1e-4)
+
+    merge_value = []
+
+    while permutation_matrix.shape[1] > remainder:
+        best_idx = sims.reshape(-1).argmax()
+        row_idx = best_idx % sims.shape[1]
+        col_idx = best_idx // sims.shape[1]
+        
+        merge_value.append(permutation_matrix[row_idx, col_idx])
+
+        if col_idx < row_idx:
+            row_idx, col_idx = col_idx, row_idx
+        
+        row_origin = original_model[row_idx]
+        col_origin = original_model[col_idx]
+        
+        permutation_matrix[:, row_idx] += permutation_matrix[:, col_idx]
+        permutation_matrix = remove_col(permutation_matrix, col_idx)
+        
+        sims[:, row_idx] = torch.minimum(sims[:, row_idx], sims[:, col_idx])
+        
+        if 'magnitudes' in metric:
+            metric['magnitudes'][row_idx] = torch.minimum(metric['magnitudes'][row_idx], metric['magnitudes'][col_idx])
+            metric['magnitudes'] = remove_col(metric['magnitudes'][None], col_idx)[0]
+        
+        if a <= 0:
+            sims[row_origin*Om:(row_origin+1)*Om, row_idx] = -torch.inf
+            sims[col_origin*Om:(col_origin+1)*Om, row_idx] = -torch.inf
+        else: sims[:, row_idx] *= a
+        sims = remove_col(sims, col_idx)
+        
+        sims[row_idx, :] = torch.minimum(sims[row_idx, :], sims[col_idx, :])
+        if a <= 0:
+            sims[row_idx, row_origin*Om:(row_origin+1)*Om] = -torch.inf
+            sims[row_idx, col_origin*Om:(col_origin+1)*Om] = -torch.inf
+        else: sims[row_idx, :] *= a
+        sims = remove_col(sims.T, col_idx).T
+
+        row_origin, col_origin = original_model[row_idx], original_model[col_idx]
+        original_model = remove_col(original_model[None, :], col_idx)[0]
+        
+        if row_origin == col_origin:
+            origin = original_model[row_idx].item()
+            budget[origin] -= 1
+
+            if budget[origin] <= 0:
+                # kill origin
+                selector = original_model == origin
+                sims[selector[:, None] & selector[None, :]] = -torch.inf
+    
+
+    unmerge = permutation_matrix
+
+    merge = permutation_matrix / (permutation_matrix.sum(dim=0, keepdim=True) + 1e-5)
+
+    merge = merge.to(sims.device)
+    unmerge = unmerge.to(sims.device)
+    
+    return merge.T, unmerge
+
 
 @click.command()
 @click.argument("model1-ft", type=str, required=True)
@@ -107,14 +212,6 @@ def main(model1_ft, model2_ft, out_path, metric, device):
     unmerges = {}
 
     for layer_name in model1_features.keys():
-        # model1_feature = model1_features[layer_name].float().to(device)
-        # model2_feature = model2_features[layer_name].float().to(device)
-
-        # model1_filtered_feature = remove_pads(model_1_attention_mask, model1_feature)
-        # model2_filtered_feature = remove_pads(model_2_attention_mask, model1_feature)
-
-        # print(model2_filtered_feature.shape)
-        # exit()
 
         print(f"Layer: {layer_name}")
 
@@ -122,13 +219,9 @@ def main(model1_ft, model2_ft, out_path, metric, device):
             (model1_features[layer_name], model2_features[layer_name]), dim=-1
         )
 
-        print(f" -> {concatenated_feature.shape}")
+        # Luke why do we have a four dimentional tensor ? 
 
-        # if "attention" in layer_name:
-        #    # how to compute the metrics here?
-        #    print(layer_name)
-        # else:
-        #    print(layer_name)
+        print(f" -> {concatenated_feature.shape}")
 
         concatenated_layer = concatenated_feature
         # TODO: one wonder about flattening the first two dims :thinking-face:
