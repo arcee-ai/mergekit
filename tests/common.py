14a72e324dbdda4879ddf214e03fb39dbd4e04e3
Elliot Stein <elliot.stein17@gmail.com>
2024-10-22 17:24:01 +0100

    Fix small error in test!

--- tests/common.py
+++ tests/common.py
@@ -46,7 +46,7 @@ def run_and_check_merge(
             index = ShardedTensorIndex.from_disk(tmpdir)
             for weight_info in arch_info.all_weights(config):
                 if weight_info.name not in index.tensor_paths:
-                    raise RuntimeError(f"Output missing tensor {tensor_name}")
+                    raise RuntimeError(f"Output missing tensor {weight_info.name}")
 
         if validate:
             validate(tmpdir)
