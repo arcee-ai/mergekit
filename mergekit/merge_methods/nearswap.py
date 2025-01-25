e8c950a97be8d99e33ead58b6ee4c2a80f1e57a6
Charles Goddard <chargoddard@gmail.com>
2025-01-24 23:30:14 -0800

    Bump version number

--- mergekit/merge_methods/nearswap.py
+++ mergekit/merge_methods/nearswap.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional
 
 import torch
 
