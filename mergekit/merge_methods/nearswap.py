0853a11aebdfa9473309a22a3a11d0322d7b4168
Charles Goddard <chargoddard@gmail.com>
2025-01-24 23:28:56 -0800

    Update copyright message

--- mergekit/merge_methods/nearswap.py
+++ mergekit/merge_methods/nearswap.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Charles Goddard
+# Copyright (C) 2025 Arcee AI
 #
 # This software is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License as
