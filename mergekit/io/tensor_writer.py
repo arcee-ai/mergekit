9017715655bfc346db0fd1f156f3799084b95b9e
Charles O. Goddard <chargoddard@gmail.com>
2025-01-24 23:45:12 -0800

    Update copyright (#497)
    Also bump dependencies.

--- mergekit/io/tensor_writer.py
+++ mergekit/io/tensor_writer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 Charles O. Goddard
+# Copyright (C) 2025 Arcee AI
 #
 # This software is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License as
@@ -121,7 +121,7 @@ class TensorWriter:
             json.dump(
                 {
                     "metadata": {
-                        "mergekit_version": "0.0.5.2",
+                        "mergekit_version": "0.0.6",
                         "total_size": self.total_size,
                     },
                     "weight_map": self.weight_map,
--- mergekit/merge_methods/nearswap.py
+++ mergekit/merge_methods/nearswap.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Charles Goddard
+# Copyright (C) 2025 Arcee AI
 #
 # This software is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License as
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional
 
 import torch
 
